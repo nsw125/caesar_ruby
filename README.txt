d99d82d96c47487d6ff6e4ff373aad9b4581130b
Nick Wegscheid <nickwegscheid@yahoo.com>
2019-03-05 10:14:59 -0500

    Init commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+This program takes a string and converts it into an encrypted message by shifting each letter a certain number down the alphabet. This number is also entered by the user. This system is the Caesar Cipher, used by Julius Caesar in his correspondence.
+
+What I learned:
+I learned how to use ASCII codes, they were pivotal to the process of changing numbers, because I had an inherit system of values related to each number, rather than creating one myself.
---
+++ caesar.rb
@@ -0,0 +1,29 @@
+def caesar_cipher(string, shift)
+    negative_shift = false
+    if shift > 25
+        shift = shift % 26
+    elsif shift < -25
+        shift = -(shift.abs % 26)
+    end
+
+    result = []
+    characters = string.split('')
+    characters.each do |letter|
+        if letter =~ /[A-Za-z]/
+            code = letter.ord + shift
+            if code.chr =~ /[^A-Za-z]/
+                if shift < 0
+                    code += 26
+                else
+                    code -= 26
+                end
+            end
+            puts code.chr
+            result.push(code.chr)
+        else
+            result.push(letter)
+        end
+    end
+    puts result.join('')
+
+end
